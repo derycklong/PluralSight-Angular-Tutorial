368a14fee2b9b21618fb1c73dbf701dd6eddda02
derycklong <derycklong@gmail.com>
2021-03-26 21:28:53 +0800

    changes to delete row

--- src/app/products/product-list-component.ts
+++ src/app/products/product-list-component.ts
@@ -70,13 +70,17 @@ export class ProductListComponent implements OnInit {
   }
 
   deleteRow(x: any):void {
-    console.log(this.products[x].productName);
-    this.products.splice(x,1);
-    //this.toDelArray.push(this.products[x].productId);
+    // console.log(this.products[x].productName);
+    // this.products.splice(x,1);
+    // //this.toDelArray.push(this.products[x].productId);
+    var index = this.products.indexOf(x);
+    this.products.splice(index,1);
+
     this.performFilter(this.listFilter);
     
     console.log(x);
-    console.log(this.products)
+    console.log(index);
+    //console.log(this.products)
   }
 
   
